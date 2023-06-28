d021bbb467d074720c1f2777c2ac9713749bdaaa
Ming Tsang <tghomg@gmail.com>
2023-06-28 14:24:09 +0100

    listen for socket and update the board after players' moves

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -1,18 +1,97 @@
-import { GameCell } from "./GameCell";
-import styles from './GameBoard.module.css'
-import { useState } from "react";
+import { GameCell } from './GameCell';
+import styles from './GameBoard.module.css';
+import { useCallback, useEffect, useState } from 'react';
+import { socket } from '../src/socket';
 
 export const GameBoard = () => {
-  const [board, setBoard] = useState(Array.from({ length: 7 }, v => Array.from({ length: 6 }, v => null)));
-    
-  return (
-    <div className={styles.gameboard}>
-      {board.map((column, colIndex)=>{
-        const columnDisplays = column.map((chess, i)=> {
-          return <GameCell key={i} name={`${colIndex} ${i}`}/>;
-        })
-        return <div key={colIndex} className={styles.boardcolumn}>{columnDisplays}</div>;
-      })}
-    </div>
+  const [board, setBoard] = useState<Array<Array<string | null>>>(
+    Array.from({ length: 7 }, (v) => Array.from({ length: 6 }, (v) => null))
   );
-}
+  const [player, setPlayer] = useState<string | undefined>();
+  const [gameoverMessage, setGameoverMessage] = useState<string | undefined>();
+
+  const updateBoard = (step: number, chessToPut: string) => {
+    setBoard((preBoard) => {
+      let newBoard = [...preBoard];
+      newBoard = newBoard.map((column, colIndex) => {
+        if (colIndex === step) {
+          const newCol = [...column];
+          const index = newCol.findIndex((chess) => chess === null);
+          if (index > -1) {
+            newCol[index] = chessToPut;
+          }
+          return newCol;
+        }
+        return column;
+      });
+      console.log(newBoard);
+      return newBoard;
+    });
+  }
+
+  const updateListener = useCallback ((args: any) => {
+    updateBoard(args.step, args.player === player ? 'O' : 'X')
+    if (args.isFinished) {
+      setGameoverMessage( args.player === player ? 'You Win' : 'You Lose')
+    }
+  }, [player]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('connected to server');
+      setPlayer(socket?.id);
+    });
+
+    socket.on('message', (args) => {
+      console.log(args);
+    });
+
+    
+
+    socket.connect();
+
+    return () => {
+      console.log('removing')
+      socket.disconnect()
+    }
+  }, []);
+
+  useEffect(()=> {
+    if (player) {
+      socket.on('update_move', updateListener);
+    }
+    return () => {
+      socket.off('update_move', updateListener)
+    }
+  }, [player, updateListener])
+
+  const columnClickHandler = (idx: number) => {
+    if (socket) {
+      socket.emit('move', idx);
+    }
+  };
+  return (
+    <>
+      <h2>{player}</h2>
+      <div className={styles.gameboard}>
+        {board.map((column, colIndex) => {
+          const columnDisplays = column.map((chess, i) => {
+            return <GameCell key={i} chess={chess} />;
+          });
+          return (
+            <div
+              key={colIndex}
+              className={styles.boardcolumn}
+              onClick={() => columnClickHandler(colIndex)}
+            >
+              {columnDisplays}
+            </div>
+          );
+        })}
+      </div>
+      {
+        gameoverMessage ? <p>{gameoverMessage}</p> : undefined
+      }
+    </>
+  );
+};
--- components/GameCell.tsx
+++ components/GameCell.tsx
@@ -1,9 +1,16 @@
 import styles from './GameCell.module.css'
 
-export const GameCell = ({name} : any) => {
+export const GameCell = ({name, chess} : any) => {
+  let chessClass = styles.chess;
+  if (chess === 'O') {
+    chessClass += ` ${styles.red}`
+  } else if (chess === 'X') {
+    chessClass += ` ${styles.yellow}`
+  }
   return (
     <div className={styles.gamecell}>
-      <div className={styles.chess}>{name}</div>
+
+      <div className={chessClass}>{name}</div>
     </div>
   );
 }
