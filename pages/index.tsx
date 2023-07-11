bb3f9d54fe2593e2b5ebe9838b28840c5b384ec6
Ming Tsang <tghomg@gmail.com>
2023-07-11 15:54:18 +0100

    passing socket as Context to Lobby

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import Lobby from '../components/Lobby';
+import { socket } from '../src/socket';
+import { SocketContext } from '../components/SocketContext';
 
 export default function Home() {
-  return <Lobby/>
+  return (
+    <SocketContext.Provider value={socket}>
+      <Lobby />
+    </SocketContext.Provider>
+  );
 }
