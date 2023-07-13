4edcff240b10aaa53c48e2dc1f7580a667f5d2fc
Ming Tsang <tghomg@gmail.com>
2023-07-13 10:28:57 +0100

    Leave Room Button

--- pages/gameRooms/[roomName].tsx
+++ pages/gameRooms/[roomName].tsx
@@ -1,5 +1,27 @@
-import { GameBoard } from "../components/GameRoom/GameBoard";
+import { Button } from '@mantine/core';
+import { GameBoard } from '../../components/GameRoom/GameBoard';
+import { SocketContext } from '../../components/SocketContext';
+import { useContext } from 'react';
+import { SocketEvents } from '@/socket/SocketEvents';
+import { useRouter } from 'next/router';
 
 export default function GameRoom() {
-  return <GameBoard />;
+  const socket = useContext(SocketContext);
+  const router = useRouter();
+
+  const leaveRoomHandler = () => {
+    if (socket && socket.connected) {
+      const { roomName } = router.query;
+      socket.emit(SocketEvents.LeaveRoom, { roomName }, () => {
+        router.back();
+      });
+    }
+  };
+
+  return (
+    <>
+      <GameBoard />
+      <Button onClick={leaveRoomHandler}>Leave</Button>
+    </>
+  );
 }
